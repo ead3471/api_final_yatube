48925db819628be396b180e5abde80bc079a5c81
ead3471 <ead3471@gmail.com>
2022-12-08 18:13:21 +0300

    Follow and Group models are added

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,6 +4,31 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name='follower')  # подписки юзера
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='following')  # подписчики юзера
+
+    def __str__(self):
+        return (f"{self.user.username}({self.user.id})->"
+                f"{self.author.username}({self.author.id})")
+
+    class Meta:
+        unique_together = ('author', 'user',)
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
@@ -11,6 +36,13 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(Group,
+                              related_name='posts',
+                              null=True,
+                              on_delete=models.SET_NULL,
+                              verbose_name='Группа',
+                              help_text=("Группа, "
+                                         "к которой будет относиться пост"))
 
     def __str__(self):
         return self.text
