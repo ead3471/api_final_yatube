2fdc7e9717eeb41873bf82c661c209abac94937c
ead3471 <ead3471@gmail.com>
2022-12-11 10:09:29 +0300

    Retrieve mixin removed from FollowingViewSet

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -10,7 +10,6 @@ from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.mixins import (CreateModelMixin,
-                                   RetrieveModelMixin,
                                    ListModelMixin)
 from rest_framework.viewsets import GenericViewSet
 
